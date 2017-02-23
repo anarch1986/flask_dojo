fa2798410df4064cc2e58ac59b9266397dcec1c5
anarch1986 <varga.tamas19860808@gmail.com>
2017-02-23 09:18:16 +0100

    flask dojo first 3 tasks done

--- flask_dojo.py
+++ flask_dojo.py
@@ -15,6 +15,7 @@ def init_db():
     db = CreateDatabase()
     try:
         db.db.connect()
+        db.db.drop_tables([Counter], safe=True)
         db.db.create_tables([Counter], safe=True)
         Counter.create(get_counter=0, post_counter=0)
         print("Database connection established.")
@@ -24,17 +25,29 @@ def init_db():
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('main.html', mode=None)
 
 
 @app.route('/request_counter')
 def get_request_counter():
-    get_update = Counter.update(get_counter=0)
+    get_update = Counter.update(
+        get_counter=Counter.get_counter + 1).where(Counter.id == 1)
+    get_update.execute()
+    return render_template('main.html', mode="get_request")
+
+
+@app.route('/request_counter', methods=['POST'])
+def post_request_counter():
+    post_update = Counter.update(
+        post_counter=Counter.post_counter + 1).where(Counter.id == 1)
+    post_update.execute()
+    return render_template('main.html', mode="post_request")
 
 
 @app.route('/statistics')
 def statistics():
-    return render_template('main.html')
+    counter = Counter.get()
+    return render_template('main.html', mode="statistics", counter=counter)
 
 if __name__ == "__main__":
     init_db()
